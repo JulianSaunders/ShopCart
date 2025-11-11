2deb075015d664ecf98a0f01374838d6719b7223
Julian Saunders <julesbsaunders@gmail.com>
2025-11-11 21:54:02 +0000

    Step3

--- src/main/scala/ShopCart.scala
+++ src/main/scala/ShopCart.scala
@@ -3,50 +3,78 @@
 object ShopCart {
 
   sealed trait Offer {
-    def calcPriceWithOffer(quantity: Int, item: Item): BigDecimal
+    def calcPriceWithOffer(quantity: Int, item: Item): Int
   }
 
-  private class BuyOneGetOneFree extends Offer {
-    override def calcPriceWithOffer(quantity: Int, item: Item): BigDecimal = {
+  class NoOffer extends Offer {
+    override def calcPriceWithOffer(quantity: Int, item: Item): Int = quantity
+  }
+
+  class BuyOneGetOneFree extends Offer {
+    override def calcPriceWithOffer(quantity: Int, item: Item): Int = {
       val redeemedOfferUnits = quantity / 2
       val remainderUnits = quantity - (redeemedOfferUnits * 2)
       val totalUnitsToBill = redeemedOfferUnits + remainderUnits
       if (redeemedOfferUnits > 0) println(s"BuyOneGetOneFree offer applied for '${item.sku}' quantity=$quantity  redeemedOfferUnits=$redeemedOfferUnits  remainderUnits=$remainderUnits  totalUnitsToBill=$totalUnitsToBill")
-      totalUnitsToBill * item.price
+      totalUnitsToBill
     }
   }
 
-  private class BuyTwoGetOneFree extends Offer {
-    override def calcPriceWithOffer(quantity: Int, item: Item): BigDecimal = {
+  class BuyTwoGetOneFree extends Offer {
+    override def calcPriceWithOffer(quantity: Int, item: Item): Int = {
       val redeemedOfferCount = quantity / 3
       val remainderUnits = quantity - (redeemedOfferCount * 3)
       val totalUnitsToBill = (redeemedOfferCount * 2) + remainderUnits
       if (redeemedOfferCount > 0) println(s"BuyTwoGetOneFree offer applied for '${item.sku}'  quantity=$quantity  redeemedOfferCount=$redeemedOfferCount  remainderUnits=$remainderUnits  totalUnitsToBill=$totalUnitsToBill")
-      totalUnitsToBill * item.price
+      totalUnitsToBill
     }
+  }
+
+  private def offerCheapestFree(m: Map[String, Int], p1: String, p2: String, products: Map[String, Item]): Map[String, Int] = {
+    if (m.isDefinedAt(p1) && m.isDefinedAt(p2) && products(p1).price != products(p2).price) {
+      val cheapest = if (products(p1).price < products(p2).price) p1 else p2
+      val freeUnits = m(p1).min(m(p2))
+      m.map { (k, v) =>
+        if (k.equals(cheapest)) {
+          println(s"Offer CheapestFree applied for $k quantity=$v ,  freeUnits=$freeUnits ,  new quantity = ${v - freeUnits}  p1=$p1 p1Price=${products(p1).price} | p2=$p2 p2Price=${products(p2).price} | cheapest=$cheapest")
+          (k, v - freeUnits)
+        } else (k, v)
+      }
+    } else m
   }
 
   class Item(val sku: String, val price: BigDecimal, val offer: Offer)
 
-  private val products = Map(
-    "apple" -> Item("apple", price = 0.60, BuyOneGetOneFree()),
-    "orange" -> Item("orange", price = 0.25, BuyTwoGetOneFree())
-  )
-
-  def calcTotal(items: List[String]): BigDecimal = {
+  def calcTotal(items: List[String], products: Map[String, Item]): BigDecimal = {
     val itemCounts = items.map(_.trim.toLowerCase).filter(products.isDefinedAt).groupMapReduce(identity)(_ => 1)(_ + _)
-    println(s"-------\nitemCounts= $itemCounts")
+    println(s"itemCounts= $itemCounts\n-------")
 
-    val sumOfItemTotalsWithOffers = itemCounts.map((key, quantity) => products(key).offer.calcPriceWithOffer(quantity, products(key))).sum
-    println(s"${itemCounts.mkString("[", ",", "]")} => £$sumOfItemTotalsWithOffers")
-    sumOfItemTotalsWithOffers
+    val itemCountsAfterOffers1 = itemCounts.map((k, v) => (k, products(k).offer.calcPriceWithOffer(v, products(k))))
+    println(s"itemCountsAfterOffers1= $itemCountsAfterOffers1\n-------")
+
+    val itemCountsAfterOffers2 = offerCheapestFree(itemCountsAfterOffers1, "apple", "banana", products)
+    println(s"itemCountsAfterOffers2= $itemCountsAfterOffers2\n-------")
+
+    val itemTotalsAfterOffers = itemCountsAfterOffers2.map((key, quantity) => (key, products(key).price * quantity))
+    println(s"${itemCounts.mkString("[", ",", "]")} => ${itemCountsAfterOffers2.mkString("[", ",", "]")} => $itemTotalsAfterOffers")
+
+    val sumItemTotals = itemTotalsAfterOffers.foldLeft(BigDecimal("0.00")) { case (a, (_, v)) => a + v }
+    println(s"£$sumItemTotals")
+    sumItemTotals
   }
 
+
   def main(args: Array[String]): Unit = {
+    val products = Map(
+      "apple" -> Item("apple", price = 0.60, BuyOneGetOneFree()),
+      "orange" -> Item("orange", price = 0.25, BuyTwoGetOneFree()),
+      "banana" -> Item("banana", price = 0.20, NoOffer())
+    )
+
     println("Welcome to the ShopCart app!!")
-    val items: List[String] = List(" Apple ", "APPLE", "Orange", "Apple", "", "InVaLiD")
+    val items: List[String] = List(" Apple ", "APPLE", "Orange", "Apple", "", "InVaLiD", "banana", "BANANA", " banana ")
     println(s"items are : ${items.mkString(", ")}\n")
-    calcTotal(items)
+    calcTotal(items, products)
   }
 }
 
--- src/test/scala/ShopCartTest.scala
+++ src/test/scala/ShopCartTest.scala
@@ -1,86 +1,160 @@
 
+import ShopCart.{BuyOneGetOneFree, BuyTwoGetOneFree, Item, NoOffer}
 import org.scalatest.funsuite.AnyFunSuite
 
 class ShopCartTest extends AnyFunSuite:
 
+  val products1 = Map(
+    "apple" -> Item("apple", price = 0.60, BuyOneGetOneFree()),
+    "orange" -> Item("orange", price = 0.25, BuyTwoGetOneFree())
+  )
+
+  val products2_bananaCheapest = Map(
+    "apple" -> Item("apple", price = 0.60, BuyOneGetOneFree()),
+    "orange" -> Item("orange", price = 0.25, BuyTwoGetOneFree()),
+    "banana" -> Item("banana", price = 0.20, NoOffer())
+  )
+
+  val products3_appleCheapest = Map(
+    "apple" -> Item("apple", price = 0.10, BuyOneGetOneFree()),
+    "orange" -> Item("orange", price = 0.25, BuyTwoGetOneFree()),
+    "banana" -> Item("banana", price = 0.20, NoOffer())
+  )
+
+  val products4_appleSamePriceAsBanana = Map(
+    "apple" -> Item("apple", price = 0.20, BuyOneGetOneFree()),
+    "orange" -> Item("orange", price = 0.25, BuyTwoGetOneFree()),
+    "banana" -> Item("banana", price = 0.20, NoOffer())
+  )
+
   test("should return 0.00 if no items") {
-    val result = ShopCart.calcTotal(List.empty)
+    val result = ShopCart.calcTotal(List.empty, products1)
     assert(result == 0.00)
   }
 
   test("should return 0.00 if blank item") {
-    val result = ShopCart.calcTotal(List(""))
+    val result = ShopCart.calcTotal(List(""), products1)
     assert(result == 0.00)
   }
 
   test("should return 0.0 for INVALID item") {
-    val result = ShopCart.calcTotal(List("INVALID"))
+    val result = ShopCart.calcTotal(List("INVALID"), products1)
     assert(result == 0.00)
   }
 
   test("should return 0.60 for ['apple'] as lowercase") {
-    val result = ShopCart.calcTotal(List("apple"))
+    val result = ShopCart.calcTotal(List("apple"), products1)
     assert(result == 0.60)
   }
 
   test("should return 0.60 for ['  apple  '] with extra spaces") {
-    val result = ShopCart.calcTotal(List("  apple  "))
+    val result = ShopCart.calcTotal(List("  apple  "), products1)
     assert(result == 0.60)
   }
   
   test("should return 0.60 for ['Apple'] as Camel case") {
-    val result = ShopCart.calcTotal(List("Apple"))
+    val result = ShopCart.calcTotal(List("Apple"), products1)
     assert(result == 0.60)
   }
 
   test("should return 0.60 for ['APPLE'] as UPPER CASE") {
-    val result = ShopCart.calcTotal(List("APPLE"))
+    val result = ShopCart.calcTotal(List("APPLE"), products1)
     assert(result == 0.60)
   }
 
   test("should return 0.6 for ['Apple', 'Apple'] with BuyOneGetOneFree offer applied, 1 free apple") {
-    val result = ShopCart.calcTotal(List.fill(2)("Apple"))
+    val result = ShopCart.calcTotal(List.fill(2)("Apple"), products1)
     assert(result == 0.6)
   }
 
   test("should return 1.20 for ['Apple', 'Apple', 'Apple'] with BuyOneGetOneFree offer applied, 1 free apple") {
-    val result = ShopCart.calcTotal(List.fill(3)("Apple"))
+    val result = ShopCart.calcTotal(List.fill(3)("Apple"), products1)
     assert(result == 1.20)
   }
 
   test("should return 1.20 for ['Apple', 'Apple', 'Apple', 'Apple'] with BuyOneGetOneFree offer applied, 2 free apples") {
-    val result = ShopCart.calcTotal(List.fill(4)("Apple"))
+    val result = ShopCart.calcTotal(List.fill(4)("Apple"), products1)
     assert(result == 1.20)
   }
 
   test("should return 0.25 for ['Orange']") {
-    val result = ShopCart.calcTotal(List("Orange"))
+    val result = ShopCart.calcTotal(List("Orange"), products1)
     assert(result == 0.25)
   }
 
   test("should return 0.50 for ['Orange', 'Orange']") {
-    val result = ShopCart.calcTotal(List.fill(2)("Orange"))
+    val result = ShopCart.calcTotal(List.fill(2)("Orange"), products1)
     assert(result == 0.50)
   }
 
   test("should return 0.50 for ['Orange', 'Orange', 'Orange'] with BuyTwoGetOneFree offer applied, 1 free orange") {
-    val result = ShopCart.calcTotal(List.fill(3)("Orange"))
+    val result = ShopCart.calcTotal(List.fill(3)("Orange"), products1)
     assert(result == 0.50)
   }
 
   test("should return 1.45 for ['Apple', 'Apple', 'Orange', 'Apple'] with BuyOneGetOneFree offer applied") {
-    val result = ShopCart.calcTotal(List("Apple", "Apple", "Orange", "Apple"))
+    val result = ShopCart.calcTotal(List("Apple", "Apple", "Orange", "Apple"), products1)
     assert(result == 1.45)
   }
 
   test("should return 1.95 for ['Apple', 'Apple', 'Orange', 'Apple', 'Orange', 'Orange', 'Orange'] with BuyOneGetOneFree offer applied AND BuyTwoGetOneFree offer applied") {
-    val result = ShopCart.calcTotal(List("Apple", "Apple", "Orange", "Apple", "Orange", "Orange", "Orange"))
+    val result = ShopCart.calcTotal(List("Apple", "Apple", "Orange", "Apple", "Orange", "Orange", "Orange"), products1)
     assert(result == 1.95)
   }
 
   test("should return 3.05 for ['INVALID', 'Apple', 'Apple', 'Orange', 'Orange', 'Apple', 'Apple', 'Apple', 'Orange', 'Orange', 'Orange', 'Orange', 'Orange', 'InVaLiD']") {
-    val result = ShopCart.calcTotal(List("INVALID", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Orange", "InVaLiD"))
+    val result = ShopCart.calcTotal(List("INVALID", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Orange", "InVaLiD"), products1)
     assert(result == 3.05)
+  }
+
+  test("should return 0.20 for ['banana'] as lowercase") {
+    val result = ShopCart.calcTotal(List("banana"), products2_bananaCheapest)
+    assert(result == 0.20)
+  }
+
+  test("should return 0.60 for ['apple', 'banana'] as lowercase") {
+    val result = ShopCart.calcTotal(List("apple", "banana"), products2_bananaCheapest)
+    assert(result == 0.60)
+  }
+
+  test("should return 1.00 for ['apple', 'banana', 'banana', 'banana'] as lowercase") {
+    val result = ShopCart.calcTotal(List("apple", "banana", "banana", "banana"), products2_bananaCheapest)
+    assert(result == 1.00)
+  }
+
+  test("should return 1.00 for ['apple', 'apple', 'banana', 'banana', 'banana'] as lowercase") {
+    val result = ShopCart.calcTotal(List("apple", "apple", "banana", "banana", "banana"), products2_bananaCheapest)
+    assert(result == 1.00)
+  }
+
+  test("should return 1.40 for ['apple', 'apple', 'banana', 'banana', 'banana'] as lowercase") {
+    val result = ShopCart.calcTotal(List.fill(3)("apple") ::: List("banana", "banana", "banana"), products2_bananaCheapest)
+    assert(result == 1.40)
+  }
+
+  test("should return 0.60 for ['apple', 'apple', 'banana', 'banana', 'banana'] as lowercase") {
+    val result = ShopCart.calcTotal(List.fill(3)("apple") ::: List("banana", "banana", "banana"), products3_appleCheapest)
+    assert(result == 0.60)
+  }
+
+  test("should return 60 for 1000 X Apples, 100 X Banana as lowercase, apples cheapest") {
+    val result = ShopCart.calcTotal(List.fill(1000)("apple") ::: List.fill(100)("banana"), products3_appleCheapest)
+    assert(result == 60)
+  }
+
+  test("should return 300 for 1000 X Apples, 100 X Banana as lowercase, bananas cheapest") {
+    val result = ShopCart.calcTotal(List.fill(1000)("apple") ::: List.fill(100)("banana"), products2_bananaCheapest)
+    assert(result == 300)
+  }
+
+  test("should return 220 for 1000 X Apples, 100 X Banana as lowercase, bananas cheapest") {
+    val result = ShopCart.calcTotal(List.fill(100)("apple") ::: List.fill(1000)("banana"), products2_bananaCheapest)
+    assert(result == 30 + 190)
+  }
+
+  test("should return 210 for 1000 X Apples, 100 X Banana as lowercase, bananas cheapest") {
+    val result = ShopCart.calcTotal(List.fill(100)("apple") ::: List.fill(1000)("banana"), products4_appleSamePriceAsBanana)
+    assert(result == 10 + 200)
   }
 
 end ShopCartTest
